43e0229065d262806a9f83fec569f237b964cd99
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-05-16 17:32:42 +0100

    initial commit

---
+++ placemark-svelte/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Header from '$lib/Header.svelte';
+</script>
+
+<Header />
+
+<div class="columns is-vcentered content">
+    <div class="column has-text-centered">
+        <img width="300" src="/map-marker-icon.png" alt="map-marker" />
+    </div>
+    <div class="column">
+        <h1 class="title">Welcome to Placemark</h1>
+        <p>Keep track of your places of interest!</p>
+    </div>
+</div>
